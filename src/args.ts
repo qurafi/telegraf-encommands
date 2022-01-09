1efb9ba8eb2a62aa19a2849f8ca8a84a50f53c2a
mhmd-22 <mohammedqurafi@gmail.com>
2022-01-09 16:49:14 +0300

    Error when passing invalid pre/post Parse results

--- src/args.ts
+++ src/args.ts
@@ -1,33 +1,34 @@
 export interface parserOptions {
 	maxargs?: number;
 
-	// remove duplicated args
+	/** Remove duplicated args */
 	removeDups?: boolean;
 
-	// used to pre-filter the raw query
-	// should return string
+	/** Used to pre-filter the raw query */
 	preParse?: (v: string) => string;
 
-	// used to filter the parsed arguments list
-	// should return array
+	//** Used to filter the parsed arguments list */
 	postParse?: (v: string[]) => string[];
 }
 
 export function parseArgs(query: string, options: parserOptions = {}): string[] {
 	if (options.preParse) {
 		const returned = options.preParse(query);
-		if (typeof returned == "string") {
-			query = returned;
+		if (typeof returned != "string") {
+			throw new TypeError("expected preParse result to be string");
 		}
+
+		query = returned;
 	}
 
 	let queries = query.trim().split(" ", options.maxargs);
 
 	if (options.postParse) {
 		const returned = options.postParse(queries);
-		if (Array.isArray(returned)) {
-			queries = returned;
+		if (!Array.isArray(returned)) {
+			throw new TypeError("expected postParse result to be array");
 		}
+		queries = returned;
 	}
 
 	// remove any empty string
