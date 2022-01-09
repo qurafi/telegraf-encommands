6049e1d283e4390f2bbf7f80e38d664c6850ed92
mhmd-22 <mohammedqurafi@gmail.com>
2022-01-09 17:07:02 +0300

    Add example.js

--- example.js
+++ example.js
@@ -1,22 +1,69 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-
 require("dotenv").config();
 const { Telegraf } = require("telegraf");
-const { TelegrafEncommands } = require("./lib");
+const { TelegrafEncommands, InvalidCommand } = require("./lib");
 
+// Example of cache store using AWS DynamoDB
 // https://github.com/mhmd-22/dynamo-kvstore
-const dynamoStore = require("dynamo-kvstore");
+// const dynamoStore = require("dynamo-kvstore");
+
+
+/** @param {ReturnType<TelegrafEncommands>} commands */
+function initializeCommands(commands) {
+
+
+	// create new command with default configs
+	commands.on("test", ({ ctx, command, query }) => {
+		return ctx.reply(`/${command} ${query}`);
+	});
+
+    // create command with custom configs
+	// Here we allow useRepliedTo option to take the replied to message as command query
+	commands.create("echo", {
+		required: false,
+		useRepliedTo: true,
+		handler: ({ ctx, reply_to, query }) => {
+			if (reply_to) {
+				return ctx.reply(`reply: ${query}`);
+			}
+		},
+	});
+
+	commands.create("test2", {
+		required: true,
+		// helpMessage: "invalid command arguments",
+		handler: ({ ctx, query }) => {
+			return ctx.reply(query);
+		},
+	});
+
+	// Reply with file details
+	commands.create("file", {
+		subTypes: ["photo", "sticker", "video", "voice", "audio", "document"],
+		required: false,
+		handler: ({ ctx, data, type }) => {
+			return ctx.replyWithMarkdownV2(`${type}: \`${JSON.stringify(data, null, 2)}\``);
+		},
+        
+        oninvalid(reason, ctx, next) {
+            console.log(reason);
+            if (reason == InvalidCommand.INVALID_MESSAGE_TYPE) {
+                return ctx.reply("File is required")
+            }
+            return next()
+        },
+	});
+}
 
 async function init() {
 	try {
 		const bot = new Telegraf(process.env.BOT_TOKEN);
 
 		const commands = TelegrafEncommands({
-			// save large bot replies to database and if user send the same command it'll refer to it
-			cacheStore: await new dynamoStore("bot-replies", {
-				endpoint: "http://localhost:7000",
-				region: "us-west-2",
-			}),
+            // Example of using custom cache store
+			// cacheStore: await new dynamoStore("bot-replies", {
+			// 	endpoint: "http://localhost:7000",
+			// 	region: "us-west-2",
+			// }),
 			replyCacheAge: 60, // one minute
 			defaults: {
 				required: true,
@@ -42,41 +89,6 @@ async function init() {
 	} catch (e) {
 		console.error(e);
 	}
-}
-
-function initializeCommands(commands) {
-	// create new command with default configs
-	commands.on("test", ({ ctx, command, query }) => {
-		return ctx.reply(`/${command} ${query}`);
-	});
-
-	// take the replied to message as command query
-	commands.create("echo", {
-		required: false,
-		useReply: true,
-		handler: ({ ctx, isReply, query }) => {
-			if (isReply) {
-				return ctx.reply(`reply: ${query}`);
-			}
-		},
-	});
-
-	commands.create("test2", {
-		required: true,
-		helpMessage: "invalid command arguments",
-		handler: ({ ctx, query }) => {
-			return ctx.reply(query);
-		},
-	});
-
-	// reply with width and height of the photo
-	commands.create("file", {
-		subTypes: ["photo", "sticker", "video", "voice", "audio", "document"],
-		required: false,
-		handler: ({ ctx, data, type }) => {
-			return ctx.reply(`${type}: \`${JSON.stringify(data)}\``);
-		},
-	});
 }
 
 init();
